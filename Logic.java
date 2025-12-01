4f304509238368337745ea023c8fefe15891c5c2
Beaulieum <beaulieum@limestone.on.ca>
2025-12-01 13:10:18 -0500

    work on fuctionality of the game

--- Logic.java
+++ Logic.java
@@ -13,22 +13,58 @@ public class Logic {
         difficulty = 1;
     } else if(response.equalsIgnoreCase("Hard")){
         difficulty = 2;
+    } else {
+        System.out.println("invalid");
     }
     int[][] lightAmount = new int[5][5];
     int[][] lightValue = new int[lightAmount.length][lightAmount.length];
+    
         for(int i=0; i < lightAmount[0].length; i++){
             for(int k=0; k < lightAmount[0].length; k++){
 
                 lightValue[i][k] = ThreadLocalRandom.current().nextInt(0,difficulty+1);
+                System.out.print(lightValue[i][k]);               
+            }
+            System.out.println();
+        }
+        boolean gameOn = true;
+         while(gameOn){
+        
+            int[] guess = new int[2]; 
+        System.out.println("Take a guess: (y x)"); 
+        guess[0] = sc.nextInt();
+        guess[1] = sc.nextInt();
+        
+
+        
+        
+        
+        onInput(lightValue, difficulty, guess);
+        updatedGame(lightValue);
+        }
+    }
+    static void onInput(int[][] lightValue, int difficulty, int[] guess){
+
+        int indexY = guess[0];
+        int indexX = guess[1];
+            
+        lightValue[indexY][indexX] += 1 % (difficulty+1); 
+        lightValue[indexY+1][indexX] += 1 % (difficulty+1); 
+        lightValue[indexY-1][indexX] += 1 % (difficulty+1);
+        lightValue[indexY][indexX+1] += 1 % (difficulty+1);
+        lightValue[indexY][indexX-1] += 1 % (difficulty+1);
+
+    }
+
+    static void updatedGame(int[][] lightValue){
+        for(int i=0; i < lightValue[0].length; i++){
+            for(int k=0; k < lightValue[0].length; k++){
                 System.out.print(lightValue[i][k]);
             }
             System.out.println();
         }
-
-        
-
-    
     }
+
 }
 
 
