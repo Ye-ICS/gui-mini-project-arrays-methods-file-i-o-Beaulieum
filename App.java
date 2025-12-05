982033295b9abf3b9964e3c49ad867b831ffcc3b
Beaulieum <beaulieum@limestone.on.ca>
2025-12-05 13:11:56 -0500

    Work on Score List

--- App.java
+++ App.java
@@ -9,15 +9,18 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 
 /**
- * Template JavaFX application.
+ * Main class for Lights out.
  */
 public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
     @Override
     public void start(Stage stage) {
         // Create components to add.
@@ -25,95 +28,89 @@ public class App extends Application {
         StackPane gameScreen = new StackPane();
         GridPane lightHolder = new GridPane();
         Label endLabel = new Label("Congratulations, you won.");
-        endLabel.setPrefSize(300,200);
+        endLabel.setPrefSize(300, 200);
         endLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
         endLabel.setAlignment(Pos.CENTER);
         Label startScreen = new Label("Lights out");
-        startScreen.setPrefSize(300,200);
+        startScreen.setPrefSize(300, 200);
         startScreen.setFont(Font.font("Arial", FontWeight.BOLD, 24));
         startScreen.setAlignment(Pos.CENTER);
         endLabel.setOpacity(0);
-        groot.setStyle("-fx-background-color: lavender;-fx-spacing: 10;"); 
+        groot.setStyle("-fx-background-color: lavender;-fx-spacing: 10;");
         int difficulty = 1;
         Button[][] buttonAmount = new Button[6][6];
-        int[][] buttonValue = Logic.randomizeLights(buttonAmount, difficulty); 
-        
-        for(int k = 0; k < buttonAmount.length; k++){
-        for(int i = 0; i < buttonAmount.length; i++){
-            Button light = new Button();
-            light.setPrefSize(40, 40);
-            light.setOpacity(0.5);
-            lightHolder.add(light,k,i);
-            buttonAmount[k][i] = light;
-        
-            int[] buttonIndex = new int[2];
+        int[][] buttonValue = Logic.randomizeLights(buttonAmount, difficulty);
 
-            buttonIndex[0] = k;
-            buttonIndex[1] = i;
+        for (int k = 0; k < buttonAmount.length; k++) {
+            for (int i = 0; i < buttonAmount.length; i++) {
+                Button light = new Button();
+                light.setPrefSize(40, 40);
+                light.setOpacity(0.5);
+                lightHolder.add(light, k, i);
+                buttonAmount[k][i] = light;
 
-                light.setOnMousePressed(event ->{
-                startScreen.setOpacity(0);
-                Logic.onInput(buttonValue, difficulty, buttonIndex);
-                changeButtons(buttonAmount, buttonIndex, buttonValue);
-                boolean endScreenChecker = Logic.checkIfComplete(buttonValue);
-                if(endScreenChecker = false){
-                    endLabel.setOpacity(1);
-                }
+                int[] buttonIndex = new int[2];
 
-            });
+                buttonIndex[0] = k;
+                buttonIndex[1] = i;
+
+                light.setOnMousePressed(event -> {
+                    startScreen.setOpacity(0);
+                    long startTime = System.currentTimeMillis();
             
+                    Logic.onInput(buttonValue, difficulty, buttonIndex);
+                    changeButtons(buttonAmount, buttonIndex, buttonValue);
 
-
-        
+                    boolean gameOn = Logic.checkIfComplete(buttonValue);
+                    if (gameOn = false) {
+                        endLabel.setOpacity(1);
+                        long endTime = System.currentTimeMillis();
+                        // Logic.readAndUpdateHighscore(startTime,endTime);
+                    }
+                });
             }
         }
-        gameScreen.getChildren().addAll(endLabel,startScreen,lightHolder);
+        gameScreen.getChildren().addAll(endLabel, startScreen, lightHolder);
         Button easyDifficuly = new Button();
-        easyDifficuly.setOnMousePressed(event ->{
-                // difficulty = 1;
+        easyDifficuly.setOnMousePressed(event -> {
+            // difficulty = 1;
         });
         Button hardDifficuly = new Button();
-        hardDifficuly.setOnMousePressed(event ->{
+        hardDifficuly.setOnMousePressed(event -> {
             // difficulty = 2;
-        });          
+        });
         groot.getChildren().add(gameScreen);
-        
 
         Scene scene = new Scene(groot, 240, 240);
         stage.setScene(scene);
         stage.setTitle("Lights Out");
         stage.show();
 
-     
     }
+
     /**
      * Changes looks and button value for light buttons around specific index
+     * 
      * @param lights
      * @param index
      * @param buttonValue
      */
     private void changeButtons(Button[][] buttonAmount, int[] index, int[][] buttonValue) {
-        for(int k = 0; k < buttonAmount.length; k++){
-        for(int i = 0; i < buttonAmount.length; i++){
-            if(buttonValue[k][i] == 0){
-                buttonAmount[k][i].setStyle("-fx-background-color: white;-fx-border-color: black;-fx-border-width: 1;");
-            }else if(buttonValue[k][i] == 1){
-                buttonAmount[k][i].setStyle("-fx-background-color: red;-fx-border-color: black;-fx-border-width: 1;");
-            }else if(buttonValue[k][i] == 1){
-                buttonAmount[k][i].setStyle("-fx-background-color: garnet;-fx-border-color: black;-fx-border-width: 1;");
+        for (int k = 0; k < buttonAmount.length; k++) {
+            for (int i = 0; i < buttonAmount.length; i++) {
+                if (buttonValue[k][i] == 0) {
+                    buttonAmount[k][i]
+                            .setStyle("-fx-background-color: white;-fx-border-color: black;-fx-border-width: 1;");
+                } else if (buttonValue[k][i] == 1) {
+                    buttonAmount[k][i]
+                            .setStyle("-fx-background-color: red;-fx-border-color: black;-fx-border-width: 1;");
+                } else if (buttonValue[k][i] == 1) {
+                    buttonAmount[k][i]
+                            .setStyle("-fx-background-color: garnet;-fx-border-color: black;-fx-border-width: 1;");
+                }
+
             }
-
-
-        }
-
 
         }
     }
-     
-     
-    private void checkAnswer(int[] buttonValue){
-        for(int i = 0;i< buttonValue.length; i++){
-            
-        }
-    }   
 }
--- Logic.java
+++ Logic.java
@@ -2,116 +2,139 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 import javafx.scene.control.Button;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Logic {
-    static void logic(){
-    
-    Scanner sc = new Scanner(System.in);
-    
-    System.out.println("Welcome to Lights out a game where you need to turn off all the lights\n\nThe lights coresponding to the one you pick will change whem it is changed");    
-    System.out.println("Would you like to play easy mode or hard mode? (Easy/Hard)");
-    String response = sc.nextLine();
-    int difficulty = 0;
-    if (response.equalsIgnoreCase("easy")){
-        difficulty = 1;
-    } else if(response.equalsIgnoreCase("Hard")){
-        difficulty = 2;
-    } else {
-        System.out.println("invalid");
-    }
-    int[][] lightAmount = new int[6][6]; // 6x6 grid for lights
-    int[][] lightValue = new int[lightAmount.length][lightAmount.length]; // value of each light "on, off..."
-    
-        for(int i=0; i < lightAmount[0].length; i++){ // generates grid with random values
-            for(int k=0; k < lightAmount[0].length; k++){
+    static void logic() {
 
-                lightValue[i][k] = ThreadLocalRandom.current().nextInt(0,difficulty+1);
-                System.out.print(lightValue[i][k] + " ");               
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(
+                "Welcome to Lights out a game where you need to turn off all the lights\n\nThe lights coresponding to the one you pick will change whem it is changed");
+        System.out.println("Would you like to play easy mode or hard mode? (Easy/Hard)");
+        String response = sc.nextLine();
+        int difficulty = 0;
+        if (response.equalsIgnoreCase("easy")) {
+            difficulty = 1;
+        } else if (response.equalsIgnoreCase("Hard")) {
+            difficulty = 2;
+        } else {
+            System.out.println("invalid");
+        }
+        int[][] lightAmount = new int[9][9]; // 6x6 grid for lights
+        int[][] lightValue = new int[lightAmount.length][lightAmount.length]; // value of each light "on, off..."
+
+        for (int i = 0; i < lightAmount[0].length; i++) { // generates grid with random values
+            for (int k = 0; k < lightAmount[0].length; k++) {
+
+                lightValue[i][k] = ThreadLocalRandom.current().nextInt(0, difficulty + 1);
+                System.out.print(lightValue[i][k] + " ");
             }
             System.out.println("\n");
         }
         boolean gameOn = true;
-        checkIfComplete(lightValue); 
-        while(gameOn){
-        
-            int[] guess = new int[2]; 
-        System.out.print("choose button: 'y x': "); 
-        guess[0] = sc.nextInt();
-        guess[1] = sc.nextInt();
-        if(guess[0] < 0 || guess[1] < 0|| guess[0] > lightAmount.length || guess[0] > lightAmount.length){
-            System.out.println("Invalid guess");
-            System.exit(0);
-        }
-        
-        onInput(lightValue, difficulty, guess);
-        updatedGame(lightValue);
         checkIfComplete(lightValue);
+        while (gameOn) {
+
+            int[] guess = new int[2];
+            System.out.print("choose button: 'y x': ");
+            guess[0] = sc.nextInt();
+            guess[1] = sc.nextInt();
+            if (guess[0] < 0 || guess[1] < 0 || guess[0] > lightAmount.length || guess[0] > lightAmount.length) {
+                System.out.println("Invalid guess");
+                System.exit(0);
+            }
+
+            onInput(lightValue, difficulty, guess);
+            updatedGame(lightValue);
+            checkIfComplete(lightValue);
         }
     }
+
     /**
      * Changes appearence of buttons deppending on specific input
+     * 
      * @param lightValue
      * @param difficulty
      * @param guess
      */
-    static void onInput(int[][] lightValue, int difficulty, int[] guess){
+    static void onInput(int[][] lightValue, int difficulty, int[] guess) {
 
         int indexY = guess[0];
         int indexX = guess[1];
-            
-        
-        lightValue[indexY][indexX] = (lightValue[indexY][indexX] + 1) % (difficulty+1); 
-        if(indexY < 5){ 
-        lightValue[indexY+1][indexX] = (lightValue[indexY+1][indexX] + 1) % (difficulty+1);
-        } 
-        if(indexY > 0){
-        lightValue[indexY-1][indexX] = (lightValue[indexY-1][indexX] + 1) % (difficulty+1);
+
+        lightValue[indexY][indexX] = (lightValue[indexY][indexX] + 1) % (difficulty + 1);
+        if (indexY < 5) {
+            lightValue[indexY + 1][indexX] = (lightValue[indexY + 1][indexX] + 1) % (difficulty + 1);
         }
-        if(indexX < 5){
-        lightValue[indexY][indexX+1] = (lightValue[indexY][indexX+1] + 1) % (difficulty+1);
+        if (indexY > 0) {
+            lightValue[indexY - 1][indexX] = (lightValue[indexY - 1][indexX] + 1) % (difficulty + 1);
         }
-        if(indexX > 0){
-        lightValue[indexY][indexX-1] = (lightValue[indexY][indexX-1] + 1) % (difficulty+1);
+        if (indexX < 5) {
+            lightValue[indexY][indexX + 1] = (lightValue[indexY][indexX + 1] + 1) % (difficulty + 1);
+        }
+        if (indexX > 0) {
+            lightValue[indexY][indexX - 1] = (lightValue[indexY][indexX - 1] + 1) % (difficulty + 1);
         }
     }
+
     /**
      * Updates map after each alteration
+     * 
      * @param lightValue
      */
-    static void updatedGame(int[][] lightValue){
-        for(int i=0; i < lightValue[0].length; i++){
-            for(int k=0; k < lightValue[0].length; k++){
+    static void updatedGame(int[][] lightValue) {
+        for (int i = 0; i < lightValue[0].length; i++) {
+            for (int k = 0; k < lightValue[0].length; k++) {
                 System.out.print(lightValue[i][k] + " ");
             }
             System.out.println("\n");
         }
     }
-
-    static boolean checkIfComplete(int[][] buttonValue){
-       boolean gameOn;
-        if(Arrays.asList(buttonValue).contains(1)){
-        gameOn = true;
-       } else if(Arrays.asList(buttonValue).contains(2)){
-        gameOn = true;
-       } else {
-        gameOn = false;
-       }
-       return gameOn;
+    /**
+     * CHecks if game is complete, returns boolean to stop game.
+     * @param buttonValue
+     * @return
+     */
+    static boolean checkIfComplete(int[][] buttonValue) {
+        boolean gameOn = false;
+        
+        for(int i = 0; i < buttonValue[0].length;i++){
+        if (!Arrays.asList(buttonValue[i]).contains(0)) {
+            gameOn = true;
+        } 
     }
-    
-    static int[][] randomizeLights(Button[][] lightAmount, int difficulty){
-    int[][] lightValue = new int[lightAmount.length][lightAmount.length]; // value of each light "on, off..."     
-    for(int i=0; i < lightAmount[0].length; i++){ // generates grid with random values    
-        for(int k=0; k < lightAmount[0].length; k++){
+        return gameOn;
+    }
 
-                lightValue[i][k] = ThreadLocalRandom.current().nextInt(0,difficulty+1);               
+    static int[][] randomizeLights(Button[][] lightAmount, int difficulty) {
+        int[][] lightValue = new int[lightAmount.length][lightAmount.length]; // value of each light "on, off..."
+        for (int i = 0; i < lightAmount[0].length; i++) { // generates grid with random values
+            for (int k = 0; k < lightAmount[0].length; k++) {
+
+                lightValue[i][k] = ThreadLocalRandom.current().nextInt(0, difficulty + 1);
             }
         }
         return lightValue;
     }
+    static void readAndUpdateHighscore(long startTime, long endTime) throws FileNotFoundException{
+        long finalTime = (endTime - startTime) / 1000;
+        File highScore = new File("HighScore.txt");
+        try{
+        FileWriter addHighScore = new FileWriter("HighScore.txt");
+        addHighScore.write(finalTime + " Seconds");
+        addHighScore.close();
+        } catch (IOException ioe){
 
+        }
+        Scanner fs = new Scanner(highScore);
+        while (fs.hasNext()){
+            String score = fs.nextLine();
+        }
+        
+    }
 
 }
-
-
